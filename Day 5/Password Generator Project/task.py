dbf0040290efb886bfe795b770d05a3a2336d59b
Srikrishna Gutlapalli <gutlapalli.srikrishna@gmail.com>
2026-06-29 12:41:40 -0400

    Day 3 Completed

--- Day 5/Password Generator Project/task.py
+++ Day 5/Password Generator Project/task.py
@@ -1,10 +1,29 @@
+import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+password = ""
+
 print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+#nr_symbols = int(input(f"How many symbols would you like?\n"))
+#nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+random_letter1 = random.choice(letters)
+random_letter1 = random.choice(letters)
+
+
+#number = random.choice(numbers)
+#symbol = random.choice(symbols)
+
+for letter in range(1, nr_letters + 1):
+    print(random_letter)
+
+
+
+
+
+
 
 
