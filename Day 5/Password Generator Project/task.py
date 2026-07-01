be67fccdf614db371d18d6302a936bf8d01e58d5
Srikrishna Gutlapalli <gutlapalli.srikrishna@gmail.com>
2026-07-01 12:05:14 -0400

    Day 7 Completed

--- Day 5/Password Generator Project/task.py
+++ Day 5/Password Generator Project/task.py
@@ -3,22 +3,22 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-password = ""
 
 print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
-#nr_symbols = int(input(f"How many symbols would you like?\n"))
-#nr_numbers = int(input(f"How many numbers would you like?\n"))
-
-random_letter1 = random.choice(letters)
-random_letter1 = random.choice(letters)
+nr_symbols = int(input(f"How many symbols would you like in your password?\n"))
+nr_numbers = int(input(f"How many numbers would you like in your password?\n"))
 
 
-#number = random.choice(numbers)
-#symbol = random.choice(symbols)
+
 
 for letter in range(1, nr_letters + 1):
-    print(random_letter)
+    print(random.choice(letters))
+for symbol in range(1, nr_symbols + 1):
+    (random.choice(symbols))
+for number in range(1, nr_numbers + 1):
+    (random.choice(numbers))
+
 
 
 
