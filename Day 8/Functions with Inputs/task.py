17812b291483b869a0f8efb33bfdeb334f5f9caf
Srikrishna Gutlapalli <gutlapalli.srikrishna@gmail.com>
2026-07-03 16:42:15 -0400

    Day 8 Completed

---
+++ Day 8/Functions with Inputs/task.py
@@ -0,0 +1,14 @@
+#def greet():
+    #print("Hello,")
+   # print("My name is ")
+   # print("Srikrishna")
+
+#greet()
+
+#name = input("Enter your name: ")
+
+def great_with_name(name):
+    print(f"Hello, {name}")
+    print("My name is ")
+    print("Srikrishna")
+great_with_name("Sam")
--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -1,8 +1,13 @@
 # Functions with input
 
-def greet_with_name(name):
-    print(f"Hello {name}")
-    print(f"How do you do {name}?")
+#def greet_with_name(name):
+    #print(f"Hello {name}")
+    #print(f"How do you do {name}?")
 
 
-greet_with_name("Jack Bauer")
+#greet_with_name("Jack Bauer")
+def great_with(name,location):
+    print(f"Hello, {name}")
+    print(f"What is it like in {location}")
+
+great_with( location="Miami", name="Srikrishna")
