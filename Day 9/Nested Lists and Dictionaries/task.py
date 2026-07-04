a24ad719389a5b072c94444dae0d40b2faf7c8fe
Srikrishna Gutlapalli <gutlapalli.srikrishna@gmail.com>
2026-07-04 15:51:15 -0400

    Day 9 Completed

---
+++ Day 9/Nested Lists and Dictionaries/task.py
@@ -0,0 +1,18 @@
+travel_log = {
+     "France": ["Paris", "Lille", "Dijon"],
+     "Germany": ["Stuttgart", "Berlin"],
+ }
+
+print("Lille")
+print(travel_log["France"][1])
+
+travel_log = {
+  "France": {
+    "cities_visited": ["Paris", "Lille", "Dijon"],
+    "total_visits": 12
+   },
+  "Germany": {
+    "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+    "total_visits": 5
+   },
+}
