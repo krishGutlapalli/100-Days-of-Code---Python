d77b16fe06b0c3eb23479147d7b9d63839a19dbe
Srikrishna Gutlapalli <gutlapalli.srikrishna@gmail.com>
2026-07-08 18:16:09 -0400

    Day 12 Completed

--- Day 12/Namespaces and Scope/task.py
+++ Day 12/Namespaces and Scope/task.py
@@ -1,10 +1,17 @@
 enemies = 1
-
-
 def increase_enemies():
     enemies = 2
     print(f"enemies inside function: {enemies}")
-
-
 increase_enemies()
 print(f"enemies outside function: {enemies}")
+def drink_potion():
+    potion_strength = 2
+    print(potion_strength)
+drink_potion()
+player_health = 10
+def game():
+    def drink_potion():
+        potion_strength = 2
+        print(player_health)
+    drink_potion()
+print(player_health)
