f47130e0da3eed60b2eef3fc08b15771e5ffbd29
Srikrishna Gutlapalli <gutlapalli.srikrishna@gmail.com>
2026-07-09 09:29:34 -0400

    Day 13 Completed

--- Day 13/Describe the Problem/task.py
+++ Day 13/Describe the Problem/task.py
@@ -6,7 +6,3 @@ def my_function():
 
 my_function()
 
-# Describe the Problem - Write your answers as comments:
-# 1. What is the for loop doing?
-# 2. When is the function meant to print "You got it"?
-# 3. What are your assumptions about the value of i?
